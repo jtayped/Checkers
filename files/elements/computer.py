4226cd79395cef66fad617d127cf18a4cce3c590
jtayped <87146558+jtayped@users.noreply.github.com>
2023-02-15 15:47:25 +0100

    ai

--- files/elements/computer.py
+++ files/elements/computer.py
@@ -5,6 +5,26 @@ class Computer:
     def __init__(self, playerColor) -> None:
         self.playerColor = playerColor
     
+    def eval(self, board):
+        player1Pieces = player2Pieces = player1Kings = player2Kings = 0
+
+        for rowIndex,row in enumerate(board):
+            for colIndex,col in enumerate(row):
+                piece = board[rowIndex][colIndex]
+                if piece != 0:
+                    if piece.color == player1Color:
+                        player1Pieces += 1
+                    elif piece.color == player2Color:
+                        player2Pieces += 1
+
+                    if piece.king:
+                        if piece.color == player1Color:
+                            player1Kings += 1
+                        elif piece.color == player2Color:
+                            player2Kings += 1
+
+        return #eval value
+
     def getMove(self, board):
         pieces = getPiecesWidthValidMoves(board, self.playerColor)
         
